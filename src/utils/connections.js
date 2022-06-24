76a801f375e36544e562ede7d51a79dbb7dd4d6f
Tahir Ahmad <tahirahmad.in@gmail.com>
2022-06-24 23:32:48 +0530

    Mainnet contract adds

--- src/utils/connections.js
+++ src/utils/connections.js
@@ -5,7 +5,7 @@ import { checkCurrentChainId } from "./../actions/smartActions";
 
 let ethContractAddress = "0x6AE737c28661D9A37ffC78Ac3e926F97b2e5d876";
 let rinkebyContractAddress = "0x14c3f86a10DDBc9Df9914dACfaEad9f859914B62";
-let bscmainContractAddress = "0x6AE737c28661D9A37ffC78Ac3e926F97b2e5d876";
+let bscmainContractAddress = "0x53Ddd24e30Ab61c4b9454EA169097Bd95602789b";
 let bsctestContractAddress = "0xB00f8f951dc0c66f5B2a0c4b2343F1dc7C896732";
 
 export const isMetaMaskInstalled = () => {
