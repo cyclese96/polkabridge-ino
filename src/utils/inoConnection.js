fddbaa07412e66e4e20edd2b33f5e0469e479ac6
Tahir Ahmad <tahirahmad.in@gmail.com>
2022-06-25 20:04:52 +0530

    Mainnet Configurations for New Contract adds

--- src/utils/inoConnection.js
+++ src/utils/inoConnection.js
@@ -4,7 +4,7 @@ import inoAbi from "./inoABI.json";
 
 let ethContractAddress = "0x6AE737c28661D9A37ffC78Ac3e926F97b2e5d876";
 let rinkebyContractAddress = "0x14c3f86a10DDBc9Df9914dACfaEad9f859914B62";
-let bscmainContractAddress = "0x53Ddd24e30Ab61c4b9454EA169097Bd95602789b";
+let bscmainContractAddress = "0xA86AB20FA75CF824E27651092ba43fE35FBBF6cb";
 let bsctestContractAddress = "0xAD2e444f72F04694370458D268A3E8B470B5cF7D";
 
 var web3 = new Web3(window.ethereum);
