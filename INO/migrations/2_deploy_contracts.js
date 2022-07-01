057d45e08d7a9d6c37601c7d1d08eb3268ebe721
baron102 <baron2020victory@yandex.com>
2022-06-30 18:34:50 -0700

    amount check when purchase

--- INO/migrations/2_deploy_contracts.js
+++ INO/migrations/2_deploy_contracts.js
@@ -1,10 +1,11 @@
 const PolkaBridgeINO = artifacts.require("PolkaBridgeINO");
 const PolkaBridgeNFT = artifacts.require("PolkaBridgeNFT");
 // const PolkaBridgeNFT = "0x3407241B3dB8CD088DA5A8b2063372aB8AA2879e";
-const owner = "0x57866ed63ca5f9744cef9aa270bd1f1dce935831";
+// const owner = "0x57866ed63ca5f9744cef9aa270bd1f1dce935831";
+const owner = "0xfEEF5F353aE5022d0cfcD072165cDA284B65772B";
 //let WETH = "0xc778417E063141139Fce010982780140Aa0cD5Ab";//test rinkeby
-let WETH = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";//main 
-// let WBNB = "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd";//test bsc
+// let WETH = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";//main 
+let WBNB = "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd";//test bsc
 
 // const URI = "https://gateway.pinata.cloud/ipfs/QmRpYmKuaNnTXXHBaUXodKu4iMKHUUuFHa8c89Df2yX2mF/{id}.json";
 
@@ -17,6 +18,6 @@ const URI = "https://gateway.ipfs.io/ipfs/QmTReaXPY3zcUvzartjpMSdzTsQ6sRFvpKMooD
 module.exports = async function (deployer) {
     await deployer.deploy(PolkaBridgeNFT, URI);
     console.log("PolkaBridgeNFT deployed at " + PolkaBridgeNFT.address)
-    // await deployer.deploy(PolkaBridgeINO, PolkaBridgeNFT.address, owner, WETH);//, URI);
-    // console.log("PolkaBridgeINO deployed at " + PolkaBridgeINO.address)
+    await deployer.deploy(PolkaBridgeINO, PolkaBridgeNFT.address, owner, WBNB);//, URI);
+    console.log("PolkaBridgeINO deployed at " + PolkaBridgeINO.address)
 };
